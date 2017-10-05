6787595624dde022ceb63649fef235e993221b06
Donovan JEAN <djean@admantic.fr>
2017-10-05 16:45:03 +0200

    Add color option (project scope) for slack attachment notification

--- lib/redmine_slack/listener.rb
+++ lib/redmine_slack/listener.rb
@@ -16,41 +16,43 @@ class SlackListener < Redmine::Hook::Listener
 			return if login.to_s == issue.author.login.to_s
 		end
 
-		# short notification
+		attachment = {}
+
+		# attachment color
+		attachment[:color] = get_attachment_color(issue.project)
+
 		if Setting.plugin_redmine_slack['short_notification'] == 'yes'
-			attachment = {}
+			# short notification
+			msg = ""
 			attachment[:mrkdwn_in] = ["text"]
 			attachment[:text] = "#{escape issue.project} | <#{object_url issue}|#{escape issue}> | *CREATION* | _#{escape issue.author}_\n"
 			attachment[:text] << "*#{I18n.t "field_status"}:* `#{escape issue.status.to_s}`"
 			attachment[:text] << " | *#{I18n.t "field_priority"}:* `#{escape issue.priority.to_s}`"
 			attachment[:text] << " | *#{I18n.t "field_due_date"}:* `#{escape issue.due_date.to_s}`" if issue.due_date
-			return speak "", channel, attachment, url
+		else
+			# long notification
+			msg = "[#{escape issue.project}] #{escape issue.author} created <#{object_url issue}|#{escape issue}>#{mentions issue.description}"
+			attachment[:text] = escape issue.description if issue.description
+			attachment[:fields] = [{
+						       :title => I18n.t("field_status"),
+						       :value => escape(issue.status.to_s),
+						       :short => true
+					       }, {
+						       :title => I18n.t("field_priority"),
+						       :value => escape(issue.priority.to_s),
+						       :short => true
+					       }, {
+						       :title => I18n.t("field_assigned_to"),
+						       :value => escape(issue.assigned_to.to_s),
+						       :short => true
+					       }]
+
+			attachment[:fields] << {
+				:title => I18n.t("field_watcher"),
+				:value => escape(issue.watcher_users.join(', ')),
+				:short => true
+			} if Setting.plugin_redmine_slack['display_watchers'] == 'yes'
 		end
-
-		# long notification
-		msg = "[#{escape issue.project}] #{escape issue.author} created <#{object_url issue}|#{escape issue}>#{mentions issue.description}"
-
-		attachment = {}
-		attachment[:text] = escape issue.description if issue.description
-		attachment[:fields] = [{
-			:title => I18n.t("field_status"),
-			:value => escape(issue.status.to_s),
-			:short => true
-		}, {
-			:title => I18n.t("field_priority"),
-			:value => escape(issue.priority.to_s),
-			:short => true
-		}, {
-			:title => I18n.t("field_assigned_to"),
-			:value => escape(issue.assigned_to.to_s),
-			:short => true
-		}]
-
-		attachment[:fields] << {
-			:title => I18n.t("field_watcher"),
-			:value => escape(issue.watcher_users.join(', ')),
-			:short => true
-		} if Setting.plugin_redmine_slack['display_watchers'] == 'yes'
 
 		speak msg, channel, attachment, url
 	end
@@ -71,21 +73,24 @@ class SlackListener < Redmine::Hook::Listener
 			return if login.to_s == journal.user.login.to_s
 		end
 
-		# short notification
+		attachment = {}
+
+		# attachment color
+		attachment[:color] = get_attachment_color(issue.project)
+
 		if Setting.plugin_redmine_slack['short_notification'] == 'yes'
-			attachment = {}
+			# short notification
+			msg = ""
 			attachment[:mrkdwn_in] = ["text"]
 			attachment[:text] = "#{escape issue.project} | <#{object_url issue}|#{escape issue}> | *UPDATE* | _#{escape journal.user}_"
 			attachment[:text] << generate_short_notification(journal)
-			return speak "", channel, attachment, url
+		else
+			# long notification
+			msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>#{mentions journal.notes}"
+			attachment = {}
+			attachment[:text] = escape journal.notes if journal.notes
+			attachment[:fields] = journal.details.map { |d| detail_to_field d }
 		end
-
-		# long notification
-		msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>#{mentions journal.notes}"
-
-		attachment = {}
-		attachment[:text] = escape journal.notes if journal.notes
-		attachment[:fields] = journal.details.map { |d| detail_to_field d }
 
 		speak msg, channel, attachment, url
 	end
@@ -133,22 +138,25 @@ class SlackListener < Redmine::Hook::Listener
 			)
 		end
 
-		# short notification
+		attachment = {}
+
+		# attachment color
+		attachment[:color] = get_attachment_color(issue.project)
+
 		if Setting.plugin_redmine_slack['short_notification'] == 'yes'
-			attachment = {}
+			# short notification
+			msg = ""
 			attachment[:mrkdwn_in] = ["text"]
 			attachment[:text] = "#{escape issue.project} | <#{object_url issue}|#{escape issue}> | *UPDATE* | _#{escape journal.user}_"
 			attachment[:text] << generate_short_notification(journal)
 			attachment[:text] << "\n"
 			attachment[:text] << ll(Setting.default_language, :text_status_changed_by_changeset, "<#{revision_url}|#{escape changeset.comments}>")
-			return speak "", channel, attachment, url
+		else
+			# long notification
+			msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>"
+			attachment[:text] = ll(Setting.default_language, :text_status_changed_by_changeset, "<#{revision_url}|#{escape changeset.comments}>")
+			attachment[:fields] = journal.details.map { |d| detail_to_field d }
 		end
-
-		# long notification
-		msg = "[#{escape issue.project}] #{escape journal.user.to_s} updated <#{object_url issue}|#{escape issue}>"
-		attachment = {}
-		attachment[:text] = ll(Setting.default_language, :text_status_changed_by_changeset, "<#{revision_url}|#{escape changeset.comments}>")
-		attachment[:fields] = journal.details.map { |d| detail_to_field d }
 
 		speak msg, channel, attachment, url
 	end
@@ -258,6 +266,12 @@ private
 		# Channel name '-' is reserved for NOT notifying
 		return nil if val.to_s == '-'
 		val
+	end
+
+	def get_attachment_color(project)
+		cf = ProjectCustomField.find_by_name("Slack notification color")
+		color = project.custom_value_for(cf).value rescue nil
+		color
 	end
 
 	def generate_short_notification(journal)
